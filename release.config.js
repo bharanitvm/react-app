07ac77b68d2ef349ebef4793300c9fd1e1031400
Bharani <bharanitvm@gmaile.com>
2021-12-21 23:19:39 +0530

    fix: upload asset fix

--- release.config.js
+++ release.config.js
@@ -6,8 +6,12 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/github",
-      { assets: [{ path: "build.zip", label: "Build" }] },
-      { assets: [{ path: "coverage.zip", label: "Coverage" }] },
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
     ],
   ],
 };
